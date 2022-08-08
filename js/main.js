36fb5ebe337bd30a4962b2f31468d5731f480e55
Md Sajib Hosen <76747449+Md-Sajib-Hosen@users.noreply.github.com>
2022-08-08 16:23:56 +0600

    adding some design

--- js/main.js
+++ js/main.js
@@ -3,13 +3,11 @@ function getTheValue() {
   // get the income
   const getIncomeField = document.getElementById('income-field');
   const Income = parseFloat(getIncomeField.value);
-  // expense
   //food expense
   const foodExpenseField = document.getElementById('food-field');
   const foodExpense = parseFloat(foodExpenseField.value);
 
   //rent expense
-
   const rentExpenseField = document.getElementById('rent-field');
   const rentExpense = parseFloat(rentExpenseField.value)
   //Clothes expense
@@ -17,26 +15,48 @@ function getTheValue() {
   const clothesExpenseField = document.getElementById('clothes-field');
   const clothesExpense = parseFloat(clothesExpenseField.value)
 
-
-
-  //Expense amount
   const totalExpense = (foodExpense + rentExpense + clothesExpense);
-
-  // set Total Expense
 
   const getExpense = document.getElementById('expenseAmount');
 
+  const getBalance = document.getElementById('balance-amount');
+
+      //Checking Condition or Validity 
+  if(Income < 0 || isNaN(Income)){
+
+    getIncomeField.value = ' ';
+    return window.alert('put the valid income')
+  }
+
+  else if(foodExpense < 0 || isNaN(foodExpense)){
+    foodExpenseField.value = ' ';
+
+    return window.alert('please put valid food expense')
+  }
+  else if(rentExpense < 0 || isNaN(rentExpense)){
+    rentExpenseField.value = '';
+    return window.alert('put valid rent expense')
+    
+  }
+  else if(clothesExpense < 0 || isNaN(clothesExpense)){
+
+    clothesExpenseField.value =' ';
+    return window.alert('put valid clothes expense')
+  }
+
+  else if(totalExpense > Income){
+    getExpense.innerText = '';
+    getBalance.innerText = ' ';
+
+    return window.alert('increase your income')
+  }
+
+
+  // set Total Expense
   getExpense.innerText = totalExpense;
 
   //set balance amount
-
-  const getBalance = document.getElementById('balance-amount');
   getBalance.innerText = Income - totalExpense;
-
-
-
-
-
 
 
 }
@@ -48,12 +68,16 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
   getTheValue();
 })
 
+
+
 //save button event handler
 
 document.getElementById('savings-btn').addEventListener('click', function () {
 
 
+function saveButton(){
 
+  
   const getParcentage = document.getElementById('savings-parcentange');
   const parcentangeValue = parseFloat(getParcentage.value);
 
@@ -65,29 +89,45 @@ document.getElementById('savings-btn').addEventListener('click', function () {
   // set savings amount 
 
   const savingAmount = balance * parcentangeValue / 100;
-
-
+  //get savings
   const getSavings = document.getElementById('saving-amount');
-  getSavings.innerText = savingAmount;
 
+  const remainingBalance = balance - savingAmount;
+  const getRemainingBalance = document.getElementById('remaining-balance');
 
+    //Checking condition or Validity
+  if( parcentangeValue < 0 || isNaN(parcentangeValue)){
+    getSavings.innerText = '';
+    getParcentage.value = ' ';
+    getRemainingBalance.innerText = ' ';
+
+    return window.alert('put a valid savings amount')
+
+  }
+
+  else if(savingAmount > balance || isNaN(savingAmount) ){
+
+  //   const getSavings = document.getElementById('saving-amount');
+   getSavings.innerText = '';
+   getParcentage.value = ' ';
+   getRemainingBalance.innerText = ' ';
+
+    return window.alert('increase your balance to save more')
+  }
 
   // const getSavings = document.getElementById('saving-amount');
-  // getSavings.innerText= savingAmount;
-
-
+  getSavings.innerText = savingAmount;
 
   // remaining balance
 
-
-  const remainingBalance = balance - savingAmount;
-  console.log('remainingBalance is', remainingBalance)
+ 
   //set ramining balance 
-  const getRemainingBalance = document.getElementById('remaining-balance');
+ 
   getRemainingBalance.innerText = remainingBalance;
 
+}
 
-
+saveButton()
 
 })
 
